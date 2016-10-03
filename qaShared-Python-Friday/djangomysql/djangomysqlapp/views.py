6b53e614266a230930350403975b24dfe6494e88
edant92 <edant92@msn.com>
2016-10-03 16:36:10 +0000

    change

--- qaShared-Python-Friday/djangomysql/djangomysqlapp/views.py
+++ qaShared-Python-Friday/djangomysql/djangomysqlapp/views.py
@@ -62,27 +62,21 @@ def get_login(request):
         # create a form instance and populate it with data from the request:
         form = LoginForm(request.POST)
         # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('/thanks/')
+	if form.is_valid():
+		subject = form.cleaned_data['subject']
+		message = form.cleaned_data['message']
+		sender = form.cleaned_data['sender']
+		cc_myself = form.cleaned_data['cc_myself']
+
+		recipients = ['info@example.com']
+		if cc_myself:
+			recipients.append(sender)
+
+		send_mail(subject, message, sender, recipients)
+		return HttpResponseRedirect('/thanks/')
 
     # if a GET (or any other method) we'll create a blank form
     else:
         form = LoginForm()
 
     return render(request, 'home.html', {'form': form})
-	
-if form.is_valid():
-    subject = form.cleaned_data['subject']
-    message = form.cleaned_data['message']
-    sender = form.cleaned_data['sender']
-    cc_myself = form.cleaned_data['cc_myself']
-
-    recipients = ['info@example.com']
-    if cc_myself:
-        recipients.append(sender)
-
-    send_mail(subject, message, sender, recipients)
-    return HttpResponseRedirect('/thanks/')
