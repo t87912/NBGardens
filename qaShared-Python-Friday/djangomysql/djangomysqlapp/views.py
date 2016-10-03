4cad56bf627d2ec70d8061f01313c0170828d3d8
KayleighB94 <bellis.kayleigh@gmail.com>
2016-10-03 16:17:14 +0100

    form code

--- qaShared-Python-Friday/djangomysql/djangomysqlapp/views.py
+++ qaShared-Python-Friday/djangomysql/djangomysqlapp/views.py
@@ -36,3 +36,39 @@ def results(request, idproduct):
     return HttpResponse(response % idproduct)
 def vote(request, idproduct):
     return HttpResponse("You're voting on Product %s." % idproduct)	
+	
+def get_year(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = YearForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = YearForm()
+
+    return render(request, 'product.html', {'form': form})
+
+def get_login(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = LoginForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = LoginForm()
+
+    return render(request, 'home.html', {'form': form})
