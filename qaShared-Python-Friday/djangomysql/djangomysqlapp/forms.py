9f53a19e1b151fae7664cc12f9f2b4cfb82761fc
KayleighB94 <bellis.kayleigh@gmail.com>
2016-10-03 16:08:36 +0100

    form code

--- qaShared-Python-Friday/djangomysql/djangomysqlapp/forms.py
+++ qaShared-Python-Friday/djangomysql/djangomysqlapp/forms.py
@@ -4,5 +4,5 @@ class YearForm(forms.Form):
     chosen_year= forms.CharField(label='Year', max_length=100)
 	
 class LoginForm(forms.Form):
-    user_name= forms.CharField(label='Username', max_length=100)
+	user_name= forms.CharField(label='Username', max_length=100)
 	pass_word= forms.CharField(label='Password', max_length=100)
