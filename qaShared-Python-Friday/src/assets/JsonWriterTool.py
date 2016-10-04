5be0a727320375738ee0078f728ffa6be7ae60bb
Littster <ameenhaq@users.noreply.github.com>
2016-10-04 15:18:09 +0000

    Created JsonWriterTool.py
    A class including a method to write to JSONfile in specified form

---
+++ qaShared-Python-Friday/src/assets/JsonWriterTool.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  4 15:03:21 2016
+
+@author: Ameen
+"""
+
+import json
+import numpy as np
+
+class TheWriterClass:
+    
+    def __init__(self):
+        
+        self.data = np.empty((10, 1)).tolist()
+        print (self.data)
+    
+    
+    def writeToFile(self, data): 
+        with open('data.txt', 'w') as outfile:
+            json.dump(data, outfile, sort_keys=True, indent=4)
+
+writer_obj = TheWriterClass()
+writer_obj.writeToFile()
