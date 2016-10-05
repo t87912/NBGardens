c7f2717b0fbbf520943e7e3bd8d14468c69fa803
Littster <ameenhaq@users.noreply.github.com>
2016-10-05 16:05:42 +0000

    Updated QueryMaker.py
    Modulated the class more

--- qaShared-Python-Friday/src/assets/QueryMaker.py
+++ qaShared-Python-Friday/src/assets/QueryMaker.py
@@ -2,7 +2,7 @@
 """
 Created on Mon Sep 26 15:20:00 2016
 
-@author: Ameen
+@author: Administrator
 """
 
 import pymysql
@@ -16,16 +16,20 @@ class QueryMaker:
         self.cursor2 = conn.cursor()
     
     def main(self):
+        self.matrixSetup()
+        self.run()
+    
+    def matrixSetup(self):
         # ============ SETUP MATRIX
         number_of_tables = 20
         max_number_attributes = 13
         
-        matrix_all_atts = np.array(np.arange(number_of_tables * max_number_attributes), dtype=(str, 35)).reshape(number_of_tables, max_number_attributes)
-        matrix_keys= np.array(np.arange(number_of_tables * max_number_attributes), dtype=(str, 35)).reshape(number_of_tables, max_number_attributes)
-        matrix_keys [:]=  matrix_all_atts[:] = ''
+        self.matrix_all_attributes = np.array(np.arange(number_of_tables * max_number_attributes), dtype=(str, 35)).reshape(number_of_tables, max_number_attributes)
+        self.matrix_keys= np.array(np.arange(number_of_tables * max_number_attributes), dtype=(str, 35)).reshape(number_of_tables, max_number_attributes)
+        self.matrix_keys[:]=  self.matrix_all_attributes[:] = ''
         # ===================== END 
         
-        
+    def run(self):    
         #show columns - 
         sqlQuery = "show tables"
         #choose date
@@ -40,7 +44,7 @@ class QueryMaker:
             table = (str (tables))
             unformatted_table_name = table[2:-3]
             table_name= (str(unformatted_table_name))
-            matrix_keys[tbl_name_counter, 0] =  matrix_all_atts[tbl_name_counter, 0] =  table_name
+            self.matrix_keys[tbl_name_counter, 0] =  self.matrix_all_attributes[tbl_name_counter, 0] =  table_name
         #        print (t)
             att1 = self.cursor2.execute("describe "+str(table_name))
             
@@ -54,12 +58,12 @@ class QueryMaker:
         #            print (str(att))
                 for line in att:
                     
-                    matrix_all_atts[tbl_name_counter, attribute_column] = att[0]
+                    self.matrix_all_attributes[tbl_name_counter, attribute_column] = att[0]
         #                print(att[3])
                     # check the attribute list for any attributes for primary or foreign keys
                     if ((str(att[3]) =='PRI') or (str(att[3]) =='MUL')):
         #                    print('key found at: ' + str(att))
-                        matrix_keys[tbl_name_counter, attribute_column] = att[0]
+                        self.matrix_keys[tbl_name_counter, attribute_column] = att[0]
                 attribute_column +=1
                 att = self.cursor2.fetchone()
                 
@@ -70,9 +74,9 @@ class QueryMaker:
         # END WHILE (OUTER)
         
         print('======= MATRIX ALL ATTRIBUTES ======== \n')
-        print (matrix_all_atts)
+        print (self.matrix_all_attributes)
         print('\n\n======= MATRIX KEYS ONLY ======== \n')
-        print (matrix_keys)
+        print (self.matrix_keys)
         
         
         user_selects = input('Enter attribute you wish to query using space delimination for multiples ')
@@ -81,15 +85,15 @@ class QueryMaker:
         for word in user_selects.split():
             print (word)
             all_selects += word + ' ' 
-            if (np.where(matrix_all_atts == word)):
+            if (np.where(self.matrix_all_attributes == word)):
         #        print (arr_index)
             
-                temp_x, y = np.where(matrix_all_atts == word)
+                temp_x, y = np.where(self.matrix_all_attributes == word)
                 x.append(temp_x)
                 print(x)
             else:
                 print ('NOT FOUND')
-        concat_selects = self.findTable(x, matrix_keys)
+        concat_selects = self.findTable(x, self.matrix_keys)
         print (concat_selects)
         self.generateQuery(all_selects, concat_selects)
 
@@ -97,7 +101,7 @@ class QueryMaker:
     def findTable(self, x, matrix_keys):
         concat_selects = ''
         for element in x:
-            element_table = matrix_keys[element, 0]
+            element_table = self.matrix_keys[element, 0]
 #            print (element_table[0])
             concat_selects +=  element_table[0] + ' '
 #            all_table_names.append(element_table[0]) 
@@ -105,7 +109,7 @@ class QueryMaker:
 
     
     def generateQuery(self, all_selects, all_table_names):
-        select_command = "SELECT " + all_selects + "FROM" + all_table_names
+        select_command = "SELECT " + all_selects + "FROM " + all_table_names
         print (select_command)
 
 
