34fe5b1b8239200b543ceb683dbe355fc1934f51
Tom Cooling <Tom.Cooling@qa.com>
2016-10-05 09:09:03 +0100

    removed some unused code from jsonwriter class

--- qaShared-Python-Friday/src/assets/JsonWriterTool.py
+++ qaShared-Python-Friday/src/assets/JsonWriterTool.py
@@ -6,19 +6,8 @@ Created on Tue Oct  4 15:03:21 2016
 """
 
 import json
-import numpy as np
 
-class TheWriterClass:
-    
-    #def __init__(self):
-        
-        #self.data = np.empty((10, 1)).tolist()
-        #print (self.data)
-    
-    
+class TheWriterClass:    
     def writeToFile(self, data): 
         with open('assets/data.json', 'w') as outfile:
             json.dump(data, outfile, sort_keys=True, indent=4)
-
-#writer_obj = TheWriterClass()
-#writer_obj.writeToFile()
