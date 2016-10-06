72a8d45dd059e28a6c33b188cecbd3f233912b41
Tom <Tom.Cooling@qa.com>
2016-10-06 20:13:42 +0100

    added docstring to json writer class

--- qaShared-Python-Friday/src/assets/JsonWriterTool.py
+++ qaShared-Python-Friday/src/assets/JsonWriterTool.py
@@ -7,7 +7,10 @@ Created on Tue Oct  4 15:03:21 2016
 
 import json
 
-class TheWriterClass:    
+class TheWriterClass:
+    """ TheWriterClass: This class has one method which takes data as a
+        parameter, in the form of a list of lists, and outputs a json file
+        in /assets/Files. """
     def writeToFile(self, data): 
         with open('assets/Files/data.json', 'w') as outfile:
             json.dump(data, outfile, sort_keys=True, indent=4)
