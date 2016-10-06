c833e4bed3ddad6c2001dae676650cc259f0be7f
Tom <Tom.Cooling@qa.com>
2016-10-06 20:15:33 +0100

    added docstring to txt writer method

--- qaShared-Python-Friday/src/assets/TxtWriterTool.py
+++ qaShared-Python-Friday/src/assets/TxtWriterTool.py
@@ -6,6 +6,8 @@ Created on Wed Oct  5 09:31:15 2016
 """
 
 def writeToTXT(listoflists):
+    """ writeToTXT: This method accepts a list of lists as a parameter and
+        writes each line to a txt file in /assets/Files. """
     with open('assets/Files/data.txt', 'w') as f:
         for x in range(0, len(listoflists)):    
             f.write(repr(listoflists[x])+"\n")
