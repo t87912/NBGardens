e919a7460ca48f69d7c59a000f09b241f56af98d
Tom Cooling <Tom.Cooling@qa.com>
2016-10-06 17:02:03 +0100

    added unit test adapter, forgot to add it before

---
+++ qaShared-Python-Friday/src/UnitTestAdapter.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  6 16:49:43 2016
+
+@author: Administrator
+"""
+
+# Test Adapter Class
+import unittest
+import pymysql
+from UserStories import AllUserStories
+from mongoDatabase.MongoDatabase import MongoDatabase
+
+class UnitTestAdapter(unittest.TestCase):
+    
+    def setupEnvironment(self):
+        # Setup allUserStories()
+        self.run_query_obj = AllUserStories.AllUserStories()
+        
+        # Setup mySQL
+        self.db = pymysql.connect("", "", "", "nbgardensds")
+        
+        # Setup MongoDB
+        self.mongoDB = MongoDatabase() # Init Mongo db
+        self.mongoDB.setDatabase(self.db) # Pass MySQL db into Mongo
+        self.conn = self.mongoDB.getConnection()
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+            
+    def test_userStory7(self):
+        self.setupEnvironment()
+        queryResults = self.run_query_obj.mongoStory1(self.db, self.conn, True, 1)
+        correctResults = [['averageProductScore', 'averageDeliveryScore', 'averageServiceScore'],
+                          [7.4, 8.75, 8.75]]
+        self.assertEqual(queryResults, correctResults)
+
+if __name__ == '__main__':
+    unittest.main()
