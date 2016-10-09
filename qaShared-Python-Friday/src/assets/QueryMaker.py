73c10da5b42db45afdcbe1cb1e4a150b7f965869
Littster <ameenhaq@users.noreply.github.com>
2016-10-09 23:31:17 +0100

    Update QueryMaker.py
    Added 'WHERE' and 'AND' clauses and dictionary to change word to corresponding operators

--- qaShared-Python-Friday/src/assets/QueryMaker.py
+++ qaShared-Python-Friday/src/assets/QueryMaker.py
@@ -6,13 +6,14 @@ Created on Mon Sep 26 15:20:00 2016
 
 import pymysql
 import numpy as np
+import re
 
 class QueryMaker:
     '''
-    The class is responsible for generating user stories in real time/ at run time. 
+    The class is responsible for generating user stories in real time/ at run time.
     It determines the user story by taking a single paramter and then generates a user
-    story by evaluating the parameter (string). It abstract elements and compares and 
-    analyses them by using a data matrix (datastructure - which stores all data from 
+    story by evaluating the parameter (string). It abstract elements and compares and
+    analyses them by using a data matrix (datastructure - which stores all data from
     the MySQL database). Although in beta this feature class is able to draw join
     between tables by deducing relatable keys.
     '''
@@ -20,8 +21,8 @@ class QueryMaker:
     def __init__(self, conn):
         '''
         Instantiating of this object executes the making of a query --> instance created
-        Responsible for setting up key cursors - sporadically used and deals with SQL conenction 
-        
+        Responsible for setting up key cursors - sporadically used and deals with SQL conenction
+
         @attention: The connection statement is an external parameter for concealing sensitive details
         '''
         #self.conn = pymysql.connect("", "", "", "nbgardensds")
@@ -33,31 +34,38 @@ class QueryMaker:
     def main(self):
         '''
         Main method - control and run sub-methods. Acts as iniaiter for other procedures.
-        
+
         @return: query: the generated user story
         '''
         self.matrixSetup()
         self.addTableNamesToMat()
-        table_in_x, all_selects = self.processUserInput()
+        table_in_x, all_selects, query_type, input_where, input_and = self.processUserInput()
         all_table_aliases, all_table_names, all_aliases = self.findTable(table_in_x)
         select_statement = self.addAliasToElements(all_selects, all_aliases)
 #        print (select_statement)
-        query = self.generateQuery(select_statement, all_table_aliases, all_table_names, table_in_x)
+        query = self.generateQuery(select_statement, all_table_aliases, all_table_names, table_in_x, query_type, input_where, input_and)
         output = self.runTest(query)
+        print (output)
         return query
+
 
     def matrixSetup(self):
         '''
         Creates matricies, to store all attributes and all the keys only in seperate matricies.
         These are then used as a local database and to evaluating the user inputs The latter
         matrix is used for common key discovery.
-        
+
         @attention: This is generic and does not require pre-set values.
-        
+
         '''
-        number_of_tables = 20
-        max_number_attributes = 13
-        # setting up dimensions of matrix (n, h x l) and set attribute char length 
+        # AUTO ====================
+        number_of_tables, max_number_attributes = self.getDBDetails()
+
+        # PREDEFINED ===============
+        # number_of_tables = 20
+        # max_number_attributes = 13
+
+        # setting up dimensions of matrix (n, h x l) and set attribute char length
         self.matrix_all_attributes = np.array(np.arange(number_of_tables * max_number_attributes), dtype=(str, 35)).reshape(number_of_tables, max_number_attributes)
         self.matrix_keys= np.array(np.arange(number_of_tables * max_number_attributes), dtype=(str, 35)).reshape(number_of_tables, max_number_attributes)
         # all cells to empty
@@ -67,7 +75,7 @@ class QueryMaker:
     def getTableData(self):
         '''
         Retrieves all data from the database and iterates through each one to process.
-        
+
         @return: tables: eahc of the tables in the database
         '''
         sqlQuery = "show tables"
@@ -77,12 +85,45 @@ class QueryMaker:
         return tables
 
 
+    def getAttributeList(self, table_name):
+        '''
+        Retrives all attributes for a given table
+
+        @param table_name: name of table from db to retrieve atributes from
+        @return: results of attributes as string
+        '''
+        results = self.cursor2.execute("describe " + table_name)
+        return results
+
+
+    def getDBDetails(self):
+        '''
+        Processes returned database data and reformats for storing in matrix. Also
+        retrieves attributes list for each table and then stores in cooresponding row.
+        Sorts all idenitifed key types in seperate matrix.
+
+        @attention: This is a crucial part of the set up
+        '''
+        tables = self.getTableData()
+        tbl_name_counter = 0
+        number_of_attributes = []
+        while tables is not None:
+            print (tables)
+            table_name = str(str(tables)[2:-3])
+            attributes_found = self.getAttributeList(str(table_name))
+            number_of_attributes.append(attributes_found)
+            attribute_list = self.cursor2.fetchone()
+            tables = self.cursor.fetchone()
+            tbl_name_counter += 1
+        return tbl_name_counter, (max(number_of_attributes))+1
+
+
     def addTableNamesToMat(self):
         '''
         Processes returned database data and reformats for storing in matrix. Also
         retrieves attributes list for each table and then stores in cooresponding row.
         Sorts all idenitifed key types in seperate matrix.
-        
+
         @attention: This is a crucial part of the set up
         '''
         tables = self.getTableData()
@@ -95,8 +136,8 @@ class QueryMaker:
             table_name= (str(unformatted_table_name))
             # store table name as header row
             self.matrix_keys[tbl_name_counter, 0] =  self.matrix_all_attributes[tbl_name_counter, 0] =  table_name
-            # get attributes            
-            results = self.cursor2.execute("describe "+str(table_name))
+            # get attributes
+            results = self.getAttributeList(str(table_name))
             attribute_list = str(results)
             attnum = attribute_list[0:1]
             attribute_list = attribute_list[2:0]
@@ -122,7 +163,7 @@ class QueryMaker:
     def viewMatricies(self):
         '''
         Print all data stored in matricies - both
-        
+
         @attention: Not needed in actual execution
         '''
         print('======= MATRIX ALL ATTRIBUTES ======== \n')
@@ -130,39 +171,61 @@ class QueryMaker:
         print('\n\n======= MATRIX KEYS ONLY ======== \n')
         print (self.matrix_keys)
 
-    
+
     def getUserInput(self, instruction):
         '''
+        ** DEPRECATED METHOD **
+
         Attempt to get a valid input if its wrong then produces error message
         and then repeeats method
-        
+
         @param instruction:the stringto print to the user
         @return: user_select is astring of valid attributes
         '''
-        # ge
+        # get user input
         user_selects = input(instruction)
         for word in user_selects.split():
+            # check if word exists
             if (np.where(self.matrix_all_attributes != word)):
                 print ('Invalid input. Please try again!')
                 self.getUserInput(instruction)
         return user_selects
-        
-    
-    
+
+
     def processUserInput(self):
         '''
-       Processes user input and extracts elements and discovers where the element is 
-       in the tables as attribute. Location is stored
-        
-        @return: coordinate_x is a list of each table that the element input is in and 
+        Processes user input and extracts elements and discovers where the element is
+        in the tables as attribute. Location is stored
+
+        @return: coordinate_x is a list of each table that the element input is in and
         all_selects is the copied input string to add alias to later
         '''
         # get user input on attributes their interessted in
         #user_selects = self.getUserInput('Enter attribute you wish to query using space delimination for multiples ')
-        user_selects = input('Enter attribute you wish to query using space delimination for multiples ')
-        print (self.matrix_all_attributes)
+        user_input = input("Please enter what you'd like to search for? \n")
+
         coordinate_x = []
         all_selects = ''
+        query_type = ''
+        input_where = ''
+        input_and = ''
+
+        # created a exachnge key dictionary to replace basic term for opertors
+        exchange_terms = { 'more than':'>', 'less than':'<', 'equal to':'='}
+
+        # substitution code - replace the words taht correspond to dictionary
+        pattern = re.compile(r'\b(' + '|'.join(exchange_terms.keys()) + r')\b')
+        user_selects = pattern.sub(lambda x: exchange_terms[x.group()], user_input)
+        # query_type = user_input.split(' ', 1)[0]
+        # user_selects = user_input.split(' ', 1)[1]
+
+        if 'where' in user_selects:
+            input_where = user_selects.split('where', 1)[1]
+            user_selects = user_selects.split('where', 1)[0]
+            if 'and' in user_selects:
+                input_where = input_where.split('and', 1)[0]
+                input_and = input_where.split('and', 1)[1]
+
         # identify and break into elements
         for word in user_selects.split():
             print (word)
@@ -175,18 +238,18 @@ class QueryMaker:
                 print(coordinate_x)
             else:
                 print ('NOT FOUND')
-        print('user typesd' + all_selects)
-        return coordinate_x, all_selects
+        print('Processing input: ' + all_selects)
+        return coordinate_x, all_selects, query_type, input_where, input_and
 
 
     def addAliasToElements(self, user_selects, all_aliases):
         '''
         this method is responsible for adding aliases to each of the attribute found
         in the user input
-        
+
         @param user_selects: a copy of the user input as string
         @param all_aliases: each alias for each of the attributes
-        @return: select_statement is a modified input string but with alias for 
+        @return: select_statement is a modified input string but with alias for
         each attribute added
         '''
         select_statement = ''
@@ -204,20 +267,26 @@ class QueryMaker:
         similar to addAliasToElements adds the alias to the tables in
         SQL like format
         retrives the table name based on the x discover - from param
-        stores the element aliases and table and tablles with aliases for 
+        stores the element aliases and table and tablles with aliases for
         generating the query
-        
+
         @param table_in_x: x = reveals table name
         @return: all_table_aliases contain the table with attached alias in
-        correct SQL syntax, all_tables_name is just a table with the tables 
-        found in location x and all_aliases is the table aliases only 
+        correct SQL syntax, all_tables_name is just a table with the tables
+        found in location x and all_aliases is the table aliases only
         '''
         all_table_aliases = []
         all_table_names = []
         all_aliases = []
-        # get table name 
+        # get table name
         for element in table_in_x:
-            element_table = (self.matrix_keys[element, 0])[0]
+            # validate the user input and see if it can be retrieved and stored
+            try:
+                element_table = (self.matrix_keys[element, 0])[0]
+            # if it cannot be found and out of bounds -> ask user for another input
+            except IndexError:
+                print("Invalid Input - Cannot be Found! Try again...")
+                self.main()
             alias = self.createTableAlias(element_table)
             table_with_alias =  element_table + ' as ' + alias
             all_aliases.append(alias)
@@ -230,12 +299,12 @@ class QueryMaker:
         '''
         creates a alias from the last and first character of the attributes
         table name.
-        
+
         @attention: 'as' is an operator thus Address is converted to 'ads'
         not 'as'
-        
+
         @param element_table: table name to be used as alias
-        @return: alias is the generated alias formed fromt eh table name that 
+        @return: alias is the generated alias formed fromt eh table name that
         the attribute belongs to
         '''
         alias = ''
@@ -247,23 +316,23 @@ class QueryMaker:
         return alias
 
 
-    def generateQuery(self, select_statement, all_table_aliases, all_table_names, table_in_x):
+    def generateQuery(self, select_statement, all_table_aliases, all_table_names, table_in_x, query_type, input_where, input_and):
         '''
         The core part of this feature, its responsible of fidn a related key between
         the elemented specified as user input. After discoevry these they are added to
         the 'ON' statement of a table join query.
         All part of the query is added up here
-        
+
         @attention: 'as' is an operator thus Address is converted to 'ads'
         not 'as'
-        
+
         @param select_statement: a string of all attriubutes user inputted with aliases
-        @param all_table_aliases: tables cooresponding to attributes chosen by user 
+        @param all_table_aliases: tables cooresponding to attributes chosen by user
         witrh aliases
-        @param all_table_names: a list of tables found in location x based on user 
+        @param all_table_names: a list of tables found in location x based on user
         chosen attributes
-        @param table_in_x: the location x of each interest table 
-        @return: query is the var containing the generated query formed by the various 
+        @param table_in_x: the location x of each interest table
+        @return: query is the var containing the generated query formed by the various
         methods
         '''
         tables_sorted = []
@@ -278,57 +347,61 @@ class QueryMaker:
         row_index_flag = 0
         found_1 = []
         found_2 = []
-        # find a related key between each table 
+        # find a related key between each table
         for element in all_table_aliases:
             # skip the first one as that acts as start node
             if (row_index_flag > 0):
                 # ensure its not a duplicate table - hasn't already been added
                 if (element not in tables_sorted):
                     join = 'JOIN '+ element
-                    
-                    # go through each attribute the table one (y) and then (x) and try to find a 
+
+                    # go through each attribute the table one (y) and then (x) and try to find a
                     # similar common key
                     for y in range (13):
-                    
+
                         for x in range (13):
                              # print ((self.matrix_keys[(table_in_x[0])[0], y]),(self.matrix_keys[(table_in_x[1])[0], x]) )
                             first_table_attribute = (self.matrix_keys[(table_in_x[0])[0], y])
                             next_table_attribute = (self.matrix_keys[(table_in_x[1])[0], x])
                             if ((first_table_attribute == next_table_attribute) and (next_table_attribute != '')):
-                                self.concatenateAliases(found_1, found_2, table_in_x, y, x)
+                                found_1, found_2 = self.concatenateAliases(found_1, found_2, table_in_x, y, x)
                                 break
                             # it may be a foreign key so extract everything beofre the underscore and
                                 # then try to deduce. if found then break out of chain
                             if ('_' in first_table_attribute):
                                 if ( ((first_table_attribute.split('_'))[1]) == next_table_attribute ):
-                                    self.concatenateAliases(found_1, found_2, table_in_x, y, x)
+                                    found_1, found_2 = self.concatenateAliases(found_1, found_2, table_in_x, y, x)
                                     break
-                    
+
                             if ('_' in next_table_attribute):
                                 if ( first_table_attribute == (( next_table_attribute.split('_'))[1])  ):
-                                    self.concatenateAliases(found_1, found_2, table_in_x, y, x)
+                                    found_1, found_2 =self.concatenateAliases(found_1, found_2, table_in_x, y, x)
                                     break
                     # store each analyseed tables
                     tables_sorted.append(element)
             # if not related then go to next attribute in table
             row_index_flag += 1
         on = ''
-        
+
         # begin adding each realted key as an ON string
         for k in range (len(found_1) ):
+
             on = ' ON ' + found_2[k] + ' = ' + found_1[k]
-        
+
         query = select + from_part + join + on
+        if input_where != '':
+            query = select + from_part + join + on + ' WHERE ' + input_where
+        if input_and != '':
+            query = select + from_part + join + on + ' WHERE ' + input_where + ' AND ' + input_and
         return query
-        
 
 
     def concatenateAliases(self, found_1, found_2, table_in_x, y, x):
         '''
         creates / adds aliases for the key of the tables and reformat in SQL format
-        
+
         @attention: method developed to eliminate duplicate coding
-        
+
         @param found_1: the related key from start table
         @param found_2: the related key from relateable table
         @param table_in_x: table name location
@@ -341,25 +414,22 @@ class QueryMaker:
         # append to list to store
         found_1.append(alias_1 + '.' +  (self.matrix_keys[(table_in_x[0])[0], y]))
         found_2.append(alias_2 + '.' +  (self.matrix_keys[(table_in_x[1])[0], x]))
-
+        return found_1, found_2
 
 
     def runTest(self, query):
         '''
-        runs the SQL command that the class has generated. this is not sent back to 
+        runs the SQL command that the class has generated. this is not sent back to
         the parent class
-        
+
         @param query: the final generated query from above class
         @param output: the output of the execution is returned
         '''
         cursor= self.conn.cursor()
         cursor.execute(query)
-        # retrives all data found 
+        # retrives all data found
         output = cursor.fetchall()
-        print (output)
         return output
-
-
 
 
 '''
@@ -375,5 +445,6 @@ class QueryMaker:
 '''
 
 #################################
-#query_maker_obj = QueryMaker()
-#query_maker_obj.main()
+# query_maker_obj = QueryMaker(conn)
+# query_maker_obj.main()
+
