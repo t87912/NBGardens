cbf9d4af918046a063ff56f928f74277acd6af20
srussell3d <s.russell.3d@outlook.com>
2016-10-10 17:37:06 +0000

    Adding CF protocols

--- qaShared-Python-Friday/djangomysql/djangomysql/settings.py
+++ qaShared-Python-Friday/djangomysql/djangomysql/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%@nn2qjqf37@j-5+)y!b3@n2fyrc9fl2l)1gk(89=ekx+3u*)1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
